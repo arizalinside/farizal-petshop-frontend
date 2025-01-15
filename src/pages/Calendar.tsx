184e2c57ae4ea85afea36428313e2dc4f83fd313
Arizal <arizalinc@gmail.com>
2025-01-15 21:59:25 +0700

    First commit

---
+++ src/pages/Calendar.tsx
@@ -0,0 +1,27 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
+
+const Calendar = () => {
+  return (
+    <>
+      <Breadcrumb pageName="Calendar" />
+      <FullCalendar
+        plugins={[dayGridPlugin, googleCalendarPlugin]}
+        initialView="dayGridMonth"
+        googleCalendarApiKey="AIzaSyAftBoRJkmd8YT1Z5t97jzB7CMG7dGEPNw"
+        events={{
+          googleCalendarId: 'id.indonesian#holiday@group.v.calendar.google.com',
+        }}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,dayGridWeek,dayGridDay',
+        }}
+      />
+    </>
+  );
+};
+
+export default Calendar;
